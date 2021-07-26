c930c86e306ca950f7543b0fd2ae3ceb2eac9447
iaxat <55215422+iaxat@users.noreply.github.com>
2021-07-26 03:04:15 -0500

    update

---
+++ DeckOfCards.py
@@ -0,0 +1,47 @@
+import random
+
+
+class Card:
+    def __init__(self, suit, face):
+        self.suit = suit
+        self.face = face
+
+
+class DeckOfCards:
+    def __init__(self):
+        self.deck = []
+        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        faces = [
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "Jack",
+            "Queen",
+            "King",
+            "Ace",
+        ]
+
+        for suit in suits:
+            for face in faces:
+                self.deck.append(Card(suit, face))
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def print_deck(self):
+        for card in self.deck:
+            print(card.face, "of", card.suit, end=", ")
+
+    # This function will return the deck with specific index
+    # getter function
+    def get_ith_card(self, i):
+        return self.deck[i]
+
+
+# i is the index of the input that this function would get
